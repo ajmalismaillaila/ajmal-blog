eafc18702d42e97f6d80217ab22a67a26468fadc
Ajmal Ismail <ajmalads05@gmail.com>
2022-10-14 22:40:04 +0530

    db changed to potgre

--- main.py
+++ main.py
@@ -12,14 +12,15 @@ from flask_gravatar import Gravatar
 from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('8BYkEfBA6O6donzWlSihBXox7C0sKR6b')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgres://znvexdxvheezio:70bb6b90197be2c3ed22ce828a7a07d65836344d5eb6aa7b4074313757047fc6@ec2-44-199-9-102.compute-1.amazonaws.com:5432/d20plaah3nhdf8')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
